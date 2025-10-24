e24d1aff9648a1fe92d08fbb3d5aef2d26c69b98
alumno26.melgarejo.juan@ipm.edu.ar <alumno26.melgarejo.juan@ipm.edu.ar>
2025-10-24 12:05:15 -0300

    Realizados los puntos de SQL.

--- BDD Re-hecha.sql
+++ BDD Re-hecha.sql
@@ -1,303 +1,146 @@
-CREATE DATABASE  IF NOT EXISTS `TPMercadoTecnico` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `TPMercadoTecnico`;
--- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: TPMercadoTecnico
--- ------------------------------------------------------
--- Server version	8.0.43-0ubuntu0.24.04.2
+/*Punto 2A, PENDIENTE*/
+/*delimiter //
+create trigger chequearAgenda before insert on TPMercadoTecnico.compra for each row
+begin
+    IF EXISTS (SELECT id_publicacion FROM servicio WHERE id_publicacion = NEW.id_publicacion) THEN
+    end if;
+end//*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Punto 2B*/
+delimiter //
+create trigger actualizarNivel after insert on TPMercadoTecnico.compra for each row
+begin
+    /*id_usuario publicacion es de quien lo publica
+    id_usuario de compra es de quien la compra*/
+    
+    IF EXISTS (SELECT id_publicacion FROM producto WHERE id_publicacion = NEW.id_publicacion) THEN
+    
+    /*Update comprador*/
+    update usuario set puntos_acumulados = puntos_acumulados + 10 where id = new.id_usuario;
+    
+    /*Update vendedor*/
+    update usuario set puntos_acumulados = puntos_acumulados + 10 where id = 
+    (select id_usuario from publicacion where publicacion.id = new.id_publicacion);
+    
+    end if;
+end//
 
---
--- Table structure for table `calendario`
---
+/*Punto 2C*/
+delimiter //
+create procedure adminPublicarContenido (in nombrePublicacion varchar(255)
+, in descripcion varchar(255), in fechaPublicacion date, in precioPublicacion DECIMAL, in stockDisponible 
+int, in idVendedor int, in estado varchar(45), out mensaje text) 
+begin
+	declare esAdmin TINYINT;
+    select es_admin into esAdmin from usuario where usuario.id = idVendedor;
+    if esAdmin = 1 then
+		INSERT INTO publicacion (nombre,descripcion,fecha_publicacion,precio,stock,id_usuario,estado)
+		VALUES (nombrePublicacion, descripcion, fechaPublicacion, precioPublicacion, stockDisponible, 
+        idVendedor, estado);
+        set mensaje="Publicacion cargada con exito";
+	else 
+		set mensaje="No se pudo cargar la publicacion ya que el vendedor no es administrador";
+    end if;
+end//
+delimiter ;
 
-DROP TABLE IF EXISTS `calendario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `calendario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `fecha` datetime NOT NULL,
-  `motivo` varchar(255) NOT NULL,
-  `id_usuario` int DEFAULT NULL,
-  `id_servicio` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `servicio_ibfk_1_idx` (`id_servicio`),
-  CONSTRAINT `calendario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `fk_calendario_1` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_publicacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `calendario`
---
+/*Punto 2D*/
+delimiter //
+create procedure comprasPorUsuarioEnCiertoPeriodo(in fecha1 date, in fecha2 date, 
+in contarServicios TINYINT, in contarProductos TINYINT, out contador int)
+begin
+	declare hayFilas boolean default 1;
+    declare cantidadProductos int default 0;
+    declare cantidadServicios int default 0;
+    
+    declare idCompraAux int;
+    declare idPublicacionCompraAux int;
+	declare comprasCursor cursor for select id, id_publicacion from compra where fecha >= fecha1 and fecha <= fecha2;
+	
+    declare continue handler for not found set hayFilas = 0;
+	
+    open comprasCursor;
+	bucle:loop
+		fetch comprasCursor into idCompraAux, idPublicacionCompraAux;
+		if hayFilas = 0 then
+			leave bucle;
+		end if;
+        
+        if contarServicios = 1 then
+			IF EXISTS (SELECT id_publicacion FROM servicio WHERE id_publicacion = idPublicacionCompraAux) THEN
+				set cantidadServicios = cantidadServicios+1;
+            end if;
+		end if;
+        
+        if contarProductos = 1 then
+			IF EXISTS (SELECT id_publicacion FROM producto WHERE id_publicacion = idPublicacionCompraAux) THEN
+				set cantidadProductos = cantidadProductos+1;
+            end if;
+        end if;
+		end loop bucle;
+	close comprasCursor;
+    set contador = cantidadProductos + cantidadServicios;
+end//
+delimiter //
 
-LOCK TABLES `calendario` WRITE;
-/*!40000 ALTER TABLE `calendario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calendario` ENABLE KEYS */;
-UNLOCK TABLES;
+/*Punto 2E*/
+create procedure colocarNotificaciones(in hourAux datetime, out Mensaje text)
+begin
+	declare hayFilas boolean default 1;
+	declare idPublicacion int;
+    declare idComprador int;
+    
+	declare serviciosCursor cursor for select id_publicacion, id_usuario from compra where 
+    id_publicacion in (select id_publicacion from servicio where id_publicacion in 
+    (select id_servicio from calendario where fecha = DATE_ADD(hourAux, INTERVAL 24 HOUR))); 
+    
+	declare continue handler for not found set hayFilas = 0;
+	open nombreCursor;
+	bucle:loop
+		fetch nombreCursor into variable1;
+		if hayFilas = 0 then
+			leave bucle;
+		end if;
+		- - codigo
+	end loop bucle;
+	close nombre_cursor;
+end
+delimiter //
+create event crearNotificacionesEvento ON SCHEDULE EVERY 1 DAY STARTS TIMESTAMP(CURRENT_DATE, '08:00:00') do
+begin
+	declare horaAux datetime;
+    set horaAux=TIMESTAMP(CURRENT_DATE, '08:00:00');
+	call colocarNotificaciones(horaAux);
+end//
+delimiter //
 
---
--- Table structure for table `compra`
---
 
-DROP TABLE IF EXISTS `compra`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `compra` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `fecha` date NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `id_usuario` int DEFAULT NULL,
-  `id_publicacion` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `id_publicacion` (`id_publicacion`),
-  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_publicacion`) REFERENCES `publicacion` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*2F*/
+delimiter //
+create event ponerEnEsperaPublicacionesViejas ON SCHEDULE EVERY 1 DAY DO
+begin
+	declare hayFilas boolean default 1;
+    declare idAux int;
+    declare fechaAux DATE;
+    declare publicacionCursor cursor for select id, fecha_publicacion from publicacion;
+    declare continue handler for not found set hayFilas = 0;
+    
+    open publicacionCursor;
+    bucle:loop
+		fetch publicacionCursor into idAux, fechaAux;
+        if hayFilas = 0 then
+			leave bucle;
+		end if;
+        if (fechaAux < DATE_SUB(CURRENT_DATE, INTERVAL 6 MONTH)) THEN
+			update publicacion set estado = 'Pendiente de revision' where id=idAux;
+		end if;
+	end loop bucle;
+    close publicacionCursor;
+end;
 
---
--- Dumping data for table `compra`
---
-
-LOCK TABLES `compra` WRITE;
-/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
-/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `dia`
---
-
-DROP TABLE IF EXISTS `dia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `dia` (
-  `id` int NOT NULL,
-  `dia` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `dia`
---
-
-LOCK TABLES `dia` WRITE;
-/*!40000 ALTER TABLE `dia` DISABLE KEYS */;
-INSERT INTO `dia` VALUES (1,'Lunes'),(2,'Martes'),(3,'Miércoles'),(4,'Jueves'),(5,'Viernes'),(6,'Sábado'),(7,'Domingo');
-/*!40000 ALTER TABLE `dia` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `nivel`
---
-
-DROP TABLE IF EXISTS `nivel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `nivel` (
-  `id` int NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `descuento` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `nivel_chk_1` CHECK (((`descuento` >= 0) and (`descuento` <= 100)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nivel`
---
-
-LOCK TABLES `nivel` WRITE;
-/*!40000 ALTER TABLE `nivel` DISABLE KEYS */;
-INSERT INTO `nivel` VALUES (1,'Bronce',15.00),(2,'Plata',30.00),(3,'Oro',45.00);
-/*!40000 ALTER TABLE `nivel` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `notificaciones`
---
-
-DROP TABLE IF EXISTS `notificaciones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notificaciones` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `mensaje` varchar(255) NOT NULL,
-  `id_usuario` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `notificaciones`
---
-
-LOCK TABLES `notificaciones` WRITE;
-/*!40000 ALTER TABLE `notificaciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notificaciones` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `producto`
---
-
-DROP TABLE IF EXISTS `producto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `producto` (
-  `id_publicacion` int NOT NULL,
-  `garantia` int DEFAULT NULL,
-  PRIMARY KEY (`id_publicacion`),
-  CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_publicacion`) REFERENCES `publicacion` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `producto`
---
-
-LOCK TABLES `producto` WRITE;
-/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `publicacion`
---
-
-DROP TABLE IF EXISTS `publicacion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `publicacion` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `descripcion` varchar(255) NOT NULL,
-  `fecha_publicacion` date NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `stock` int NOT NULL,
-  `id_usuario` int DEFAULT NULL,
-  `estado` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `publicacion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `publicacion`
---
-
-LOCK TABLES `publicacion` WRITE;
-/*!40000 ALTER TABLE `publicacion` DISABLE KEYS */;
-INSERT INTO `publicacion` VALUES (1,'Set de lápices de grafito profesional','Set de 12 lápices de grafito de diferentes durezas (2H a 8B), ideales para dibujo artístico, bocetos y diseño técnico.','2025-10-20',25.90,90,1,'Activo'),(2,'Cuaderno A4','Cuaderno tamaño A4 con tapa dura y hojas rayadas de 90g. Perfecto para clases y apuntes profesionales.','2025-10-19',35.00,75,2,'Activo'),(3,'Mochila escolar','Mochila ergonómica con múltiples compartimentos, refuerzos en las costuras y diseño impermeable.','2025-10-18',150.90,40,3,'Activo'),(4,'Bolígrafo retráctil','Bolígrafo con tinta de gel azul y punta de 0.7 mm. Escritura suave y secado rápido, ideal para uso diario.','2025-10-17',12.75,200,4,'Activo'),(5,'Calculadora científica','Calculadora científica de 240 funciones, pantalla de dos líneas y memoria programable para uso académico.','2025-10-16',89.99,60,5,'Activo'),(6,'Resaltadores pastel','Set de 6 resaltadores con tonos suaves pastel, tinta resistente al agua y punta biselada para mayor precisión.','2025-10-15',45.50,120,6,'Activo'),(7,'Brochas de papel','Brocha para papel de plástico con gancho de metal incluido','2025-10-20',50.00,50,1,'En pausa'),(8,'aaaa','aaaa','2025-10-20',11.00,11,1,'Activo'),(9,'affff','aaaa','2025-10-20',11.00,11,1,'Activo');
-/*!40000 ALTER TABLE `publicacion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `servicio`
---
-
-DROP TABLE IF EXISTS `servicio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `servicio` (
-  `id_publicacion` int NOT NULL,
-  PRIMARY KEY (`id_publicacion`),
-  CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`id_publicacion`) REFERENCES `publicacion` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `servicio`
---
-
-LOCK TABLES `servicio` WRITE;
-/*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `servicio_has_dias`
---
-
-DROP TABLE IF EXISTS `servicio_has_dias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `servicio_has_dias` (
-  `servicio_id` int NOT NULL,
-  `dia_id` int NOT NULL,
-  PRIMARY KEY (`servicio_id`,`dia_id`),
-  KEY `dia_id` (`dia_id`),
-  CONSTRAINT `servicio_has_dias_ibfk_1` FOREIGN KEY (`servicio_id`) REFERENCES `servicio` (`id_publicacion`),
-  CONSTRAINT `servicio_has_dias_ibfk_2` FOREIGN KEY (`dia_id`) REFERENCES `dia` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `servicio_has_dias`
---
-
-LOCK TABLES `servicio_has_dias` WRITE;
-/*!40000 ALTER TABLE `servicio_has_dias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `servicio_has_dias` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `apellido` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contrasena` varchar(100) NOT NULL,
-  `puntos_acumulados` int DEFAULT NULL,
-  `es_admin` tinyint(1) NOT NULL DEFAULT '0',
-  `id_nivel` int DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  KEY `usuario_ibfk_1` (`id_nivel`),
-  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_nivel`) REFERENCES `nivel` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Juan','Pérez','juan.perez@email.com','contraseña123',100,0,1),(2,'María','Gómez','maria.gomez@email.com','passw0rd456',200,1,2),(3,'Carlos','López','carlos.lopez@email.com','abc123',150,0,3),(4,'Ana','Martínez','ana.martinez@email.com','1234password',50,0,2),(5,'Luis','Rodríguez','luis.rodriguez@email.com','mypassword789',250,1,3),(6,'Tomas','elrepelotudodegarayzar','noteresbales@gmail.com','jaja',0,0,1),(17,'Tomas','NoRegalesGolesGarayzar','aguanteSanJose@gmail.com','jijo',0,0,1),(18,'a','a','asas@a','aaa',0,0,1);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-10-22  9:39:46
+delimiter ;
+    
+        
+    
+	
